92797487225e44b1db166bf6f3a2aa875e00053a
Josh Caponigro <joshcap@email.unc.edu>
2024-01-04 07:01:40 -0500

    Fix handler

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
+import sys
 import select
 import socket
 import struct
 from socketserver import StreamRequestHandler
 
 from constants import SOCKS_VERSION, ReplyCode
-from utils import send_socks_response, parse_socks_request
+from utils import generate_failed_reply
 
 
 class SocksProxy(StreamRequestHandler):
@@ -23,20 +24,44 @@ class SocksProxy(StreamRequestHandler):
             return
 
         # Request handling
-        version, cmd, address_type, dest_address, dest_port = parse_socks_request(self.connection)
+        version, cmd, _, address_type = struct.unpack("!BBBB", self.connection.recv(4))
+
         if version != SOCKS_VERSION:
             # Incorrect SOCKS version
             return
 
+        if address_type == 1:  # IPv4
+            address = socket.inet_ntoa(self.connection.recv(4))
+        elif address_type == 3:  # Domain name
+            domain_length = self.connection.recv(1)[0]
+            address = self.connection.recv(domain_length)
+            address = socket.gethostbyname(address)
+        port = struct.unpack("!H", self.connection.recv(2))[0]
+
         try:
             if cmd == 1:
-                self.handle_connect(dest_address, dest_port)
+                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                remote.connect((address, port))
+                bind_address = remote.getsockname()
             else:
                 # Unsupported command
-                self.send_reply(ReplyCode.COMMAND_NOT_SUPPORTED)
+                self.server.close_request(self.request)
+
+            addr = struct.unpack("!I", socket.inet_aton(bind_address[0]))[0]
+            port = bind_address[1]
+            reply = struct.pack("!BBBBIH", SOCKS_VERSION, 0, 0, 1, addr, port)
+
         except Exception as e:
             # General SOCKS server failure
-            self.send_reply(ReplyCode.GENERAL_SOCKS_SERVER_FAILURE)
+            reply = generate_failed_reply(address_type, ReplyCode.GENERAL_SOCKS_SERVER_FAILURE)
+
+        self.connection.sendall(reply)
+
+        # establish data exchange
+        if reply[1] == 0 and cmd == 1:
+            self.relay_data(self.connection, remote)
+
+        self.server.close_request(self.request)
 
     def authenticate(self, methods: bytes) -> bool:
         # Implement authentication logic
@@ -44,39 +69,28 @@ class SocksProxy(StreamRequestHandler):
         self.connection.sendall(struct.pack("!BB", SOCKS_VERSION, 0))
         return True
 
-    def handle_connect(self, dest_address: str, dest_port: int) -> None:
-        # Establish a connection to the destination
-        try:
-            remote_socket = socket.create_connection((dest_address, dest_port))
-            local_address, local_port = remote_socket.getsockname()
-            self.send_reply(ReplyCode.SUCCEEDED, local_address, local_port)
-
-            # Start relaying data
-            self.relay_data(self.connection, remote_socket)
-        except socket.error:
-            self.send_reply(ReplyCode.CONNECTION_REFUSED)
-
     def relay_data(
         self, client_socket: socket.socket, remote_socket: socket.socket
     ) -> None:
-        while True:
-            # Wait until either socket has data to read
-            r, w, e = select.select([client_socket, remote_socket], [], [])
+        try:
+            while True:
+                # wait until client or remote is available for read
+                r, w, e = select.select([client_socket, remote_socket], [], [])
 
-            if client_socket in r:
-                data = client_socket.recv(4096)
-                if len(data) == 0:  # No more data to read
-                    break
-                remote_socket.sendall(data)
+                if client_socket in r:
+                    data = client_socket.recv(4096)
+                    if remote_socket.send(data) <= 0:
+                        break
 
-            if remote_socket in r:
-                data = remote_socket.recv(4096)
-                if len(data) == 0:  # No more data to read
-                    break
-                client_socket.sendall(data)
+                if remote_socket in r:
+                    data = remote_socket.recv(4096)
+                    if client_socket.send(data) <= 0:
+                        break
 
-    def send_reply(
-        self, reply_code: ReplyCode, address: str = "0.0.0.0", port: int = 0
-    ) -> None:
-        # Send a SOCKS reply to the client
-        send_socks_response(self.connection, reply_code, address, port)
+        except BrokenPipeError as e:
+            print(f"Caught BrokenPipeError: {e}", file=sys.stderr)
+        except ConnectionResetError as e:
+            print(f"Caught ConnectionResetError: {e}", file=sys.stderr)
+        finally:
+            client_socket.close()
+            remote_socket.close()
