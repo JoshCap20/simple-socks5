7b461d0f32928570d8164e91d2995156de940773
Josh Caponigro <joshcap@email.unc.edu>
2024-01-04 08:10:44 -0500

    Fix exiting program

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import sys
+import signal
+
 from server import SocksProxy
 from constants import HOST_NAME, PORT_NUMBER
 from server import ThreadingTCPServer
 
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     with ThreadingTCPServer((HOST_NAME, PORT_NUMBER), SocksProxy) as server:
         print(f"SOCKS server started on {HOST_NAME}:{PORT_NUMBER}")
-        server.serve_forever()
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print("Server shutting down.")
+
+
+def signal_handler(signum, frame):
+    print("Signal received, shutting down the server.")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
