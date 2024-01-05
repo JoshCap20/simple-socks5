fc37769526df6e7e27ed745b99bd37db54fd020b
Josh Caponigro <joshcap@email.unc.edu>
2024-01-05 04:58:54 -0500

    Optimize RequestHandler

--- request_handler.py
+++ request_handler.py
@@ -1,18 +1,27 @@
 import struct
 import socket
 
-from constants import SOCKS_VERSION
+from constants import SOCKS_VERSION, AddressTypeCodes
+from logger import logger
 
 
 class RequestHandler:
     @staticmethod
     def handle_handshake(connection: socket.socket) -> bool:
-        version, nmethods = struct.unpack("!BB", connection.recv(2))
-        if version != SOCKS_VERSION:
-            return False
+        try:
+            header = connection.recv(2)
+            if len(header) < 2:
+                return False
 
-        methods = connection.recv(nmethods)
-        return RequestHandler._authenticate(connection, methods)
+            version, nmethods = struct.unpack("!BB", header)
+            if version != SOCKS_VERSION:
+                return False
+
+            methods = connection.recv(nmethods)
+            return RequestHandler._authenticate(connection, methods)
+        except socket.error as e:
+            logger.error(f"Socket error during handshake: {e}")
+            return False
 
     @staticmethod
     def _authenticate(connection: socket.socket, methods) -> bool:
@@ -22,18 +31,45 @@ class RequestHandler:
 
     @staticmethod
     def parse_request(connection: socket.socket) -> tuple:
-        version, cmd, _, address_type = struct.unpack("!BBBB", connection.recv(4))
-        return version, cmd, address_type
+        try:
+            header = connection.recv(4)
+            if len(header) < 4:
+                return None, None, None
+
+            version, cmd, _, address_type = struct.unpack("!BBBB", header)
+            return version, cmd, address_type
+        except socket.error as e:
+            logger.error(f"Socket error during request parsing: {e}")
+            return None, None, None
 
     @staticmethod
-    def parse_address_and_port(
-        connection: socket.socket, address_type: int
-    ) -> socket.socket:
-        if address_type == 1:  # IPv4
-            address = socket.inet_ntoa(connection.recv(4))
-        elif address_type == 3:  # Domain name
-            domain_length = connection.recv(1)[0]
-            address = connection.recv(domain_length)
-            address = socket.gethostbyname(address)
-        port = struct.unpack("!H", connection.recv(2))[0]
-        return address, port
+    def parse_address_and_port(connection: socket.socket, address_type: int) -> tuple:
+        try:
+            if address_type == AddressTypeCodes.IPv4:
+                address = socket.inet_ntoa(connection.recv(4))
+            elif address_type == AddressTypeCodes.DOMAIN_NAME:
+                domain_length = connection.recv(1)[0]
+                address = connection.recv(domain_length)
+                address = socket.gethostbyname(address)
+            elif address_type == AddressTypeCodes.IPv6:
+                address = socket.inet_ntop(socket.AF_INET6, connection.recv(16))
+            else:
+                return None, None
+
+            port = struct.unpack("!H", connection.recv(2))[0]
+            return address, port
+        except socket.error as e:
+            logger.error(f"Socket error during address and port parsing: {e}")
+            return None, None
+
+    @staticmethod
+    def get_address_type(connection: socket.socket) -> AddressTypeCodes | None:
+        _, _, address_type = RequestHandler.parse_request(connection)
+        if address_type == 1:
+            return AddressTypeCodes.IPv4
+        elif address_type == 3:
+            return AddressTypeCodes.DOMAIN_NAME
+        elif address_type == 4:
+            return AddressTypeCodes.IPv6
+        else:
+            return None
