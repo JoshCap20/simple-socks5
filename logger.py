204a4b552a33d9ce6b4d828261bfa333af9b5785
Josh Caponigro <joshcap@email.unc.edu>
2024-01-06 03:49:14 -0500

    Logger updates

--- logger.py
+++ logger.py
@@ -1,19 +1,19 @@
 import logging
 
-
 class LogColors:
     RED = '\033[91m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     WHITE = '\033[97m'
+    GREY = '\033[90m'
     RESET = '\033[0m'
 
 class ColorFormatter(logging.Formatter):
     COLORS = {
         logging.ERROR: LogColors.RED,
         logging.WARNING: LogColors.YELLOW,
-        logging.INFO: LogColors.GREEN,
-        logging.DEBUG: LogColors.WHITE
+        logging.INFO: LogColors.WHITE,
+        logging.DEBUG: LogColors.GREY
     }
 
     def format(self, record):
@@ -23,13 +23,14 @@ class ColorFormatter(logging.Formatter):
             message = color + message + LogColors.RESET
         return message
 
-def get_logger(name: str) -> logging.Logger:
+def get_logger(name: str, verbose=True) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     # Console Handler for logging info and above
     c_handler = logging.StreamHandler()
-    c_handler.setLevel(logging.DEBUG)
+    console_level = logging.DEBUG if verbose else logging.INFO
+    c_handler.setLevel(console_level)
     c_format = ColorFormatter("[%(asctime)s] - [%(levelname)s] - %(message)s")
     c_handler.setFormatter(c_format)
 
