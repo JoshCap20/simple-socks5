7609e2067a52e16d52935c2ed3d9e198c451434b
Josh Caponigro <joshcap@email.unc.edu>
2024-01-07 06:58:43 -0500

    simplify server

--- src/server.py
+++ src/server.py
@@ -10,7 +10,7 @@ from .utils import (
     generate_connection_refused_reply,
     generate_host_unreachable_reply,
     generate_succeeded_reply,
-    generate_socket,
+    generate_address_from_socket,
 )
 from .logger import get_logger
 from .models import Request, Address
@@ -36,6 +36,7 @@ class TCPProxyServer(StreamRequestHandler):
     https://docs.python.org/3/library/socketserver.html#socketserver.StreamRequestHandler
     """
 
+    client_address: Address
     connection: socket.socket
     server: ThreadingTCPServer
 
@@ -55,41 +56,38 @@ class TCPProxyServer(StreamRequestHandler):
             self.server.shutdown_request(self.request)
             return
 
-        conn_request: Request = request_handler.parse_request()
+        dst_request: Request = request_handler.parse_request()
 
+        self.client_address = generate_address_from_socket(self.connection)
         reply: bytes | None = None
 
         logger.info(
-            f"Connection Established: {conn_request.address.name}, {conn_request.address.ip}, {conn_request.address.port}, {conn_request.address.address_type}"
+            f"Connection Established: {dst_request.address} <-> {self.client_address}"
         )
 
         try:
-            match conn_request.command:
+            match dst_request.command:
                 case CommandCodes.CONNECT.value:
                     # CONNECT
-                    reply = self.handle_connect(conn_request.address)
+                    reply = self.handle_connect(dst_request.address)
 
                 case CommandCodes.BIND.value:
                     # TODO: BIND
-                    reply = self.handle_bind(conn_request.address)
+                    reply = self.handle_bind(dst_request.address)
 
                 case CommandCodes.UDP_ASSOCIATE.value:
                     # UDP ASSOCIATE
-                    reply = self.handle_udp_associate(conn_request.address)
+                    reply = self.handle_udp_associate(dst_request.address)
 
                 case _:
                     # Invalid command
                     reply = generate_command_not_supported_reply()
 
         except ConnectionRefusedError:
-            logger.error(
-                f"Connection refused: {conn_request.address.name}, {conn_request.address.ip}:{conn_request.address.port}"
-            )
+            logger.error(f"Connection refused: {dst_request.address}")
             reply = generate_connection_refused_reply()
         except socket.gaierror:
-            logger.error(
-                f"Host unreachable: {conn_request.address.name}, {conn_request.address.ip}:{conn_request.address.port}"
-            )
+            logger.error(f"Host unreachable: {dst_request.address}")
             reply = generate_host_unreachable_reply()
         except Exception as e:
             logger.error(f"Exception: {e}")
@@ -103,34 +101,32 @@ class TCPProxyServer(StreamRequestHandler):
                     logger.error(f"Error sending reply: {e}")
             self.server.shutdown_request(self.request)
 
-    def handle_connect(self, address: Address) -> None:
+    def handle_connect(self, dst_address: Address) -> None:
         """
         Handles CONNECT command.
         """
         # Allocate port for TCP relay
-        tcp_relay = TCPRelay(self.connection, address)
-        bind_address = tcp_relay.get_allocated_address()
+        tcp_relay = TCPRelay(self.connection, dst_address)
 
         # Send reply with bind address and port
         success_reply = generate_succeeded_reply(
-            address.address_type, bind_address[0], bind_address[1]
+            dst_address.address_type, *tcp_relay.get_proxy_address()
         )
         self.connection.sendall(success_reply)
 
         # Start TCP relay
         tcp_relay.listen_and_relay()
 
-    def handle_udp_associate(self, address: Address) -> None:
+    def handle_udp_associate(self, dst_address: Address) -> None:
         """
         Handles UDP ASSOCIATE command.
         """
         # Allocate port for UDP relay
-        udp_relay = UDPRelay(address)
-        allocated_port = udp_relay.get_allocated_port()
+        udp_relay = UDPRelay(self.connection, dst_address)
 
         # Send reply with allocated port and server IP
         success_reply = generate_succeeded_reply(
-            address.address_type, self.server.server_address[0], allocated_port  # type: ignore
+            dst_address.address_type, *udp_relay.get_proxy_address()
         )
         self.connection.sendall(success_reply)
 
