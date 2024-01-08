c1d98f8bb9fc150be80d368827ed7a6c143400c7
Josh Caponigro <joshcap@email.unc.edu>
2024-01-08 05:43:20 -0500

    better log templates

--- src/utils/logs.py
+++ src/utils/logs.py
@@ -4,10 +4,10 @@ Standardized log messages.
 import string
 
 connection_established_template = string.Template(
-    "CONN | $name | $ip:$port | $address_type"
+    "CONNECTION | $name | $ip:$port | $address_type"
 )
 connection_closed_template = string.Template(
-    "TERM | ($src_domain_name) $src_ip:$src_port <-> ($dst_domain_name) $dst_ip:$dst_port"
+    "CLOSED | ($src_domain_name) $src_ip:$src_port <-> ($dst_domain_name) $dst_ip:$dst_port"
 )
 base_relay_template = string.Template(
     "RELAY | $protocol | $src_ip:$src_port -> $dst_ip:$dst_port | $data_size bytes"
